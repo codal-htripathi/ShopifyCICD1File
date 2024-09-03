53f4eacf0db2b18d434f3b2e9105e0787c1c7eb3
codal-htripathi <htripathi@codal.com>
2024-09-03 14:58:56 +0530

    Adding scripts

---
+++ scripts/watch-theme.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const themeKit = require('@shopify/themekit');
+const { argv } = require('yargs');
+const {
+  paths: { srcDir }
+} = require('../app.config');
+/**
+ * Required flags are set via environment variables in the form of `THEMEKIT_<flag>`.
+ * eg: `THEMEKIT_STORE`, `THEMEKIT_PASSWORD`, etc.
+ */
+(async () => {
+  await themeKit.command('watch', {
+    dir: argv.dir || srcDir
+  });
+})();
